ac451347b9ec5d8094877664ed2c1d0215deb215
gjx <252932110@qq.com>
2025-07-01 09:05:00 +0800

    change rspack -> vite7

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
